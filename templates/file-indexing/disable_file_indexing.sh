06a4aeeb81fd9e22f90977f3510402d9cd96f846
Brad Cowie <brad@wand.net.nz>
2022-01-24 16:36:12 +1300

    Improve disable_file_indexing.sh.
      * Don't run for root user or when DISPLAY unset.
      * Only disable baloo when session is KDE.
      * Disable baloo when config file already exists.

--- templates/file-indexing/disable_file_indexing.sh
+++ templates/file-indexing/disable_file_indexing.sh
@@ -1,23 +1,21 @@
 # {{ ansible_managed }}
 
+# Exit if run as root
+[ "$(id -u)" -eq 0 ] && return 0
+
+# Exit if there isn't a DISPLAY attached
+[ -z "${DISPLAY}" ] && return 0
+
 ### Disable tracker file indexing ###
 systemctl --user mask {{ desktop_tracker_services | join(" ") }} 2> /dev/null
 
 if [ -x "/usr/bin/tracker" ]; then
-  /usr/bin/tracker daemon -t > /dev/null
+    /usr/bin/tracker daemon -t > /dev/null
 fi
 
 ### Disable baloo file indexing ###
-CONFIG_DIR="${HOME}/.config"
-
-if [ -n "${XDG_CONFIG_HOME}" ]; then
-  CONFIG_DIR="${XDG_CONFIG_HOME}"
-fi
-
-if [ ! -d "${CONFIG_DIR}" ]; then
-  mkdir -p "${CONFIG_DIR}"
-fi
-
-if [ ! -f "${CONFIG_DIR}/baloofilerc" ]; then
-  echo -e "[Basic Settings]\nIndexing-Enabled=false" > "${CONFIG_DIR}/baloofilerc"
+if [ -x "/usr/bin/balooctl" ]; then
+    if ! /usr/bin/balooctl status 2>&1 | grep "Baloo is currently disabled" >/dev/null; then
+        /usr/bin/balooctl disable
+    fi
 fi
