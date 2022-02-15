eaf8d2a49d8da0a0d442e8f8772fbc4b26f4914d
Brad Cowie <brad@wand.net.nz>
2022-02-15 14:50:21 +1300

    Move pulseaudio state/config directory to local storage.

---
+++ templates/profile.d/pulseaudio.state.sh
@@ -0,0 +1,20 @@
+# {{ ansible_managed }}
+
+# Exit if run as root
+[ "$(id -u)" -eq 0 ] && return 0
+
+pulseaudio_local_path="/var/tmp/pulseaudio-${USER}"
+
+if [ -d "${pulseaudio_local_path}" ]; then
+    # verify existing dir is suitable
+    if [ ! -G "${pulseaudio_local_path}" ] || [ ! -w "${pulseaudio_local_path}" ]; then
+        # else, make a new/secure one with mktemp
+        pulseaudio_local_path="$(mktemp -d "${pulseaudio_local_path}-XXXXXX")"
+    fi
+else
+    mkdir -p "${pulseaudio_local_path}"
+    chmod 700 "${pulseaudio_local_path}"
+fi
+
+export PULSE_CONFIG_PATH="${pulseaudio_local_path}"
+export PULSE_STATE_PATH="${pulseaudio_local_path}"
---
+++ templates/systemd/generators/pulseaudio.state.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# {{ ansible_managed }}
+
+. /etc/profile.d/pulseaudio.state.sh
+
+if [ -n "${PULSE_CONFIG_PATH}" ]; then
+    echo "PULSE_CONFIG_PATH=${PULSE_CONFIG_PATH}"
+fi
+
+if [ -n "${PULSE_STATE_PATH}" ]; then
+    echo "PULSE_STATE_PATH=${PULSE_STATE_PATH}"
+fi
